417503ee12ceacd7da95cefcdc34628f4d7e865c
zhangpeng <zhangpeng@baixing.com>
2014-04-30 23:48:31 +0800

    Prepare for pushing code to upstream.

--- src/core.js
+++ src/core.js
@@ -4,32 +4,3 @@ var root = root || window
 var _ = root._
 var $ = root.Zepto || root.jQuery || root.$
 var _ext = {}
-
-//config
-/*
-_ext.config = {
-	debug: _.str.include(location.href, 'debug=1') || _.str.include(location.hash, 'debug=1'),
-	selAction: ''
-};
-_ext.config._ini = function () {
-	//todo: window.onhashchange to update _.config.debug
-	this.get = function (key) {
-		return _.isString(key) && key ? sessionStorage.getItem(key) || localStorage.getItem(key) || '' : false;  //to be improved
-	};
-	this.set = function (key, value, bSession) {
-		if (_.isString(key)) {
-			(bSession ? sessionStorage : localStorage).setItem(key, '' + value);
-		} else {
-			return false;
-		}
-	};
-	this.del = function (key) {
-		if (_.isString(key) && key) {
-			sessionStorage.removeItem(key);
-			localStorage.removeItem(key);
-		} else {
-			return false;
-		}
-	};
-};
-*/
