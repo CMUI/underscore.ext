0acd1f86d40b00c3afa27b5648147ad46846090d
zhangpeng <zhangpeng@baixing.com>
2014-05-07 10:16:17 +0800

    Update exports fn.

--- src/core.js
+++ src/core.js
@@ -16,22 +16,24 @@ void function (window, _ext) {
 		if (!key || !_.isString(key) || !apiSet || !_.isObject(apiSet)) return false
 		if (key === 'root') {
 			//{apiSet}.xxx => _.xxx
-			_.each(apiSet, function (n, i) {
-				checkKey(i)
-				_[i] = n
+			_.each(apiSet, function (value, key) {
+				exportKey(key, value)
 			})
 		} else {
 			//{apiSet}.xxx => _.{key}.xxx
+			exportKey(key, apiSet)
+		}
+
+		function exportKey(key, apiSet) {
 			if (checkKey(key)) {
 				_.extend(_[key], apiSet)
 			} else {
 				_[key] = apiSet
 			}
 		}
-
 		function checkKey(key) {
 			if (_[key]) {
-				//warn if going to modify existed key
+				//warn if going to modify existed key unintentionally
 				var knownKeysToBeExtended = ['template', 'str']
 				if (!_.include(knownKeysToBeExtended, key)) {
 					console.warn('_ already has key: ' + key)
