68516f2ebfa4db5166c9352bd0fde12a317cfe2b
zhangpeng <zhangpeng@baixing.com>
2014-05-12 23:24:49 +0800

    Update `action` module and its unit tests.

--- src/action.js
+++ src/action.js
@@ -1,5 +1,27 @@
 
 ////////////////////  action  ////////////////////
+//an easy and lazy solution for click-event-binding
+
+//step 1:
+//use `_.action.extend(actionList)` to define some actions
+//format of actionList:
+//{"action-name": callback, [...]}
+
+//step 2:
+//create a element and append to dom:
+//`<a href="#action-name" data-action>link</a>`
+//or `<a href="#" data-action="action-name">link</a>`
+//or `<button data-action="action-name">btn</button>`
+
+//step 3:
+//you've done everything. click the element to trigger the callback.
+//the callback is called on the element as its context.
+
+//one more thing:
+//use `_.action.trigger("action-name")` to call the callback manually.
+//you can use the optional second param to specify the callback's context:
+//`_.action.trigger("action-name", context)`
+
 void function (window, _ext) {
 	'use strict'
 
@@ -15,26 +37,26 @@ void function (window, _ext) {
 			var elem = this
 			var $elem = _.$(elem)
 			//get action
-			var action = $elem.data('action') || _.url.getHashFromLink(elem)
-			if (!action) {
+			var actionName = $elem.data('action') || _.url.getHashFromLink(elem)
+			if (!actionName) {
 				console.warn('No action assigned!')
 			} else {
-				action = _.str.stripHash(action)
-				if (!action || action === 'none') {
+				actionName = _.str.stripHash(actionName)
+				if (!actionName || actionName === 'none') {
 					console.info('Empty action. Do nothing.')
 				} else {
-					_handle(action, elem)
+					_handle(actionName, elem)
 				}
 			}
 		})
 	}
-	function _handle(action, context) {
-		var fn = _actionList[action]
+	function _handle(actionName, context) {
+		var fn = _actionList[actionName]
 		if (_.isFunction(fn)) {
-			console.log('action: ' + action)
+			console.log('executing action: ' + actionName)
 			fn.call(context || window)
 		} else {
-			console.error('Not found callback of action: ' + action)
+			console.error('Not found callback of action: ' + actionName)
 		}
 	}
 
@@ -42,14 +64,14 @@ void function (window, _ext) {
 	action._actionList = _actionList
 
 	//api
-	action.extend = function (o) {
-		if (_.isPlainObject(o)) {
-			_.extend(_actionList, o)
+	action.extend = function (actionSet) {
+		if (_.isPlainObject(actionSet)) {
+			_.extend(_actionList, actionSet)
 		}
 	}
-	action.trigger = function (s, context) {
-		if (_.isString(s)) {
-			_handle(s, context)
+	action.trigger = function (actionName, context) {
+		if (_.isString(actionName)) {
+			_handle(actionName, context)
 		}
 	}
 
--- test/test-action.js
+++ test/test-action.js
@@ -1,12 +1,12 @@
 describe('Action', function () {
-	var test = ''
-	var actions = {}
+	var testKey = ''
+	var actionSet = {}
 	var _actionList = _.action._actionList
 	beforeEach(function () {
-		test = ''
+		testKey = ''
 	})
 	afterEach(function () {
-		test = ''
+		testKey = ''
 		delete _actionList.foo
 		delete _actionList.bar
 	})
@@ -14,32 +14,74 @@ describe('Action', function () {
 	describe('DOM binding', function () {
 		var $wrapper, $link
 		before(function () {
-			$wrapper = $('<div id="test"><a href="#" data-action>test action</a></div>').appendTo('body')
+			$wrapper = $('<div id="test"><a href="#" data-action>test action</a></div>')
+				.css({position: 'absolute', top: '-50px'})
+				.appendTo('body')
 			$link = $wrapper.find('a')
 		})
 		after(function () {
 			$wrapper.remove()
 		})
-		it('trigger action via `a`\'s href', function (done) {
+		it('get action name from `href`', function (done) {
 			$link.attr('href', '#foo')
-			actions.foo = function () {
+			_actionList.foo = function () {
+				testKey = 'test-foo'
+			}
+			$link.click()
+			_.delay(function () {
+				expect(testKey).to.equal('test-foo')
+				done()
+			}, 50)
+		})
+		it('get action name from `href` - context points to the link', function (done) {
+			$link.attr('href', '#foo')
+			_actionList.foo = function () {
 				expect(this).to.equal($link[0])
 				done()
 			}
 			$link.click()
 		})
-		//
+		it('get action name from `data-action`', function (done) {
+			$link.attr('data-action', 'bar')
+			_actionList.bar = function () {
+				testKey = 'test-bar'
+			}
+			$link.click()
+			_.delay(function () {
+				expect(testKey).to.equal('test-bar')
+				done()
+			}, 50)
+		})
+		it('get action name from `data-action` - context points to the link', function (done) {
+			$link.attr('data-action', 'bar')
+			_actionList.bar = function () {
+				expect(this).to.equal($link[0])
+				done()
+			}
+			$link.click()
+		})
+		it('accept `data-action` value as a hash', function (done) {
+			$link.attr('data-action', '#bar')
+			_actionList.bar = function () {
+				testKey = 'test-bar'
+			}
+			$link.click()
+			_.delay(function () {
+				expect(testKey).to.equal('test-bar')
+				done()
+			}, 50)
+		})
 	})
-	describe('API', function () {
+	describe('APIs', function () {
 		describe('_.action.extend()', function () {
 			it('do basic functionality', function () {
 				expect(_actionList).to.be.deep.equal({})
-				actions = {
+				actionSet = {
 					foo: function () {},
 					bar: function () {}
 				}
-				_.action.extend(actions)
-				expect(_actionList).to.be.deep.equal(actions)
+				_.action.extend(actionSet)
+				expect(_actionList).to.be.deep.equal(actionSet)
 			})
 			it('do nothing if input other types', function () {
 				expect(_actionList).to.be.deep.equal({})
@@ -54,19 +96,33 @@ describe('Action', function () {
 
 		describe('_.action.trigger()', function () {
 			it('do basic functionality', function () {
-				actions = {
+				actionSet = {
 					foo: function () {
-						test = 'test-foo'
+						testKey = 'test-foo'
 					},
 					bar: function () {
-						test = 'test-bar'
+						testKey = 'test-bar'
 					}
 				}
-				_.action.extend(actions)
+				_.action.extend(actionSet)
 				_.action.trigger('foo')
-				expect(test).to.equal('test-foo')
+				expect(testKey).to.equal('test-foo')
 				_.action.trigger('bar')
-				expect(test).to.equal('test-bar')
+				expect(testKey).to.equal('test-bar')
+			})
+			it('call callback on the specified context', function () {
+				var context = {}
+				actionSet = {
+					foo: function () {
+						expect(this).to.equal(context)
+					},
+					bar: function () {
+						expect(this).to.equal(_)
+					}
+				}
+				_.action.extend(actionSet)
+				_.action.trigger('foo', context)
+				_.action.trigger('bar', _)
 			})
 		})
 	})
