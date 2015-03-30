10f08de024e14565fce28747369c0d244956c7c8
cssmagic <cssmagic.cn@gmail.com>
2015-03-30 16:21:55 +0800

    Add building system. Update README. Complete PR #48.

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,43 @@
+'use strict'
+
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var uglify = require('gulp-uglifyjs')
 var del = require('del')
 
-gulp.task('default', ['uglify'])
+var scripts = [
+	'./src/adapter-dist-trad/_intro.js',
+	'./src/adapter-dist-trad/var.js',
+	'./src/adapter-dist-trad/_defense.js',
+	'./src/core.js',
+	'./src/str-backup.js',
+	'./src/str.js',
+	'./src/root.js',
+	'./src/ua.js',
+	'./src/url.js',
+	'./src/dom.js',
+	'./src/adapter-mod-action/_intro.js',
+	'./bower_components/action/src/action.js',
+	'./src/adapter-mod-action/_outro.js',
+	'./src/adapter-mod-template/_intro.js',
+	'./bower_components/underscore-template/src/underscore-template.js',
+	'./src/adapter-mod-template/config.js',
+	'./src/adapter-mod-template/_outro.js',
+	'./src/adapter-dist-trad/_outro.js'
+]
 
-//clean old files
-gulp.task('clean', function(cb) {
-	return del('./dist/*.js', cb)
+gulp.task('default', ['clean'], function () {
+	gulp.start('js')
 })
 
-//Merge JS files
-gulp.task('concat', ['clean'], function() {
-	return gulp.src([
-				 './src/adapter-dist-trad/_intro.js',
-				 './src/adapter-dist-trad/var.js',
-				 './src/adapter-dist-trad/_defense.js',
-				 './src/core.js',
-				 './src/str-backup.js',
-				 './src/str.js',
-				 './src/root.js',
-				 './src/ua.js',
-				 './src/url.js',
-				 './src/dom.js',
-				 './src/adapter-mod-action/_intro.js',
-				 './bower_components/action/src/action.js',
-				 './src/adapter-mod-action/_outro.js',
-				 './src/adapter-mod-template/_intro.js',
-				 './bower_components/underscore-template/src/underscore-template.js',
-				 './src/adapter-mod-template/config.js',
-				 './src/adapter-mod-template/_outro.js',
-				 './src/adapter-dist-trad/_outro.js'
-			   ])
-			   .pipe(concat('underscore.ext.js'))
-			   .pipe(gulp.dest('./dist/'))
+gulp.task('clean', function () {
+	del('./dist/*.js')
 })
 
-//Compress the Js file
-gulp.task('uglify', ['concat'], function() {
-	return gulp.src('./dist/underscore.ext.js')
-	    	   .pipe(uglify('underscore.ext.min.js'))
-			   .pipe(gulp.dest('./dist'))
+gulp.task('js', function() {
+	gulp.src(scripts)
+		.pipe(concat('underscore.ext.js'))
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify('underscore.ext.min.js'))
+		.pipe(gulp.dest('./dist'))
 })
