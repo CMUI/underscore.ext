5f8a45b4009d202a8f4964be1c963640fcda02a3
cssmagic <cssmagic.cn@gmail.com>
2015-06-24 11:01:07 +0800

    Remove `variable` config in template module, resolve #47.

--- src/adapter-mod-template/config.js
+++ src/adapter-mod-template/config.js
@@ -2,10 +2,6 @@
 var _config = {
 	//compatible with ejs
 	interpolate : /<%-([\s\S]+?)%>/g,
-	escape      : /<%=([\s\S]+?)%>/g,
-
-	//to avoid use `with` in compiled templates
-	//see: https://github.com/cssmagic/blog/issues/4
-	variable: 'data'
+	escape      : /<%=([\s\S]+?)%>/g
 }
 _.extend(_.templateSettings, _config)
